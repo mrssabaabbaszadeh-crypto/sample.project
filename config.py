41d60c5ee15c27706b81896805a604cbd7335fd4
saba <mrs.saba.abbaszadeh@gmail.com>
2026-08-19 21:46:31 +0330

    remove extra files and keep 3 files

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv() 
-
-database = os.environ.get('database', 'Store')
-database_config = {'user': os.environ.get('user'), 'password': os.environ.get('password'), 'host': os.environ.get('host')}
-
-print(database_config)
